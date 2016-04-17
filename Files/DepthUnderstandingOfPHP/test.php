47f9d47e9d42abadc3eb55967e2cc1dac01d9a9d
helen <731873664@qq.com>
2016-04-17 18:38:12 +0800

    《深入理解PHP--高级技巧、面向对象与核心技术》--书籍配套代码

---
+++ Files/DepthUnderstandingOfPHP/test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Author: helen
+ * CreateTime: 2015/11/7 11:33
+ * description:
+ */
+    phpinfo();
+    /*$array = array(
+        'helen'=>'huihui'
+    );
+    $a = 10/0;
+    var_dump($a);*/
