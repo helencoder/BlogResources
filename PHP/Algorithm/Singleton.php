e64288fe999ce171d23689f9d36f869b684d8161
helencoder <731873664@qq.com>
2016-05-14 19:34:32 +0800

    单例模式PHP实现

--- PHP/Algorithm/Singleton.php
+++ PHP/Algorithm/Singleton.php
@@ -9,9 +9,19 @@
 class Singleton{
     private static $_instance;
 
+    /**
+     *  私有化构造函数，防止外界实例化对象
+     */
     private function __construct(){}
+    /**
+     *  私有化克隆函数，防止外界克隆对象
+     */
     private function __clone(){}
 
+    /**
+     *  静态方法, 单例统一访问入口
+     *  @return  object  返回对象的唯一实例
+     */
     public static function getInstance()
     {
         if(self::$_instance == null){
