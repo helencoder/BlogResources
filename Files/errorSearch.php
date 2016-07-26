ef1167563c8886376ab5643bcfce2af8b90f03fe
helen <shixi_hailun3@staff.weibo.com>
2016-07-26 22:37:21 +0800

    错误异常处理方式（重定向查找错误）

--- Files/errorSearch.php
+++ Files/errorSearch.php
@@ -7,37 +7,29 @@
 
 // 打开错误信息
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
+// 启用自定义错误控制
 set_error_handler('customErrorHandler');
+
 // 自定义错误处理函数
 function customErrorHandler($error, $error_string, $filename, $line, $symbols)
 {
-    $error_no_arr = array(
+    $error_data = array(
         1 => 'ERROR', 2 => 'WARNING', 4 => 'PARSE', 8 => 'NOTICE',
         16 => 'CORE_ERROR', 32 => 'CORE_WARNING', 64 => 'COMPILE_ERROR',
         128 => 'COMPILE_WARNING', 256 => 'USER_ERROR', 512 => 'USER_WARNING',
         1024 => 'USER_NOTICE', 2047 => 'ALL', 2048 => 'STRICT'
     );
-    if (in_array($error, array(1, 2, 4))) {
-        //var_dump($error, $error_string, $filename, $line, $symbols);
+    $error_no = array(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2047, 2048);
+    if (in_array($error, $error_no)) {
         $msg = $filename . ':' . $line . '&nbsp' . $error_string;
-        //dieByError($msg);
-        throw new Exception($error_string);
+        $errorSearch = $error_string . ' php';
+        throw new Exception($errorSearch);
     }
 }
-
-//显示错误信息
-function dieByError($msg)
-{
-    echo $msg;
-    exit();
-}
-
 
 // 错误自动定向查询
 try {
     $b = 4 / 0;
-    $message = '';
-    $code = 12;
 } catch (Exception $e) {
     header("Location: http://stackoverflow.com/search?q=" . $e->getMessage());
 } finally {
